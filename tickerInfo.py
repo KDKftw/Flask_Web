fb5c67368e6e832cdf11a9dfade53ea0e4fa785d
KDKftw <ooo.kadoun@gmail.com>
2021-10-10 23:34:29 +0200

    stonksdate, daycount, tickers added

--- tickerInfo.py
+++ tickerInfo.py
@@ -13,23 +13,41 @@ class Trade():
         return(total_paid)
 
     def print_values(self):
-        print("ticker_name= " + self.name, "|| ", "buy_date= " + self.buyDate, "|| " , "amount_of_shares= " + str(self.amount), "|| " , "total= " + str(self.total), "|| " , "fee= " + str(self.fee),"|| "
-              , "total_paid= " + str(self.total_paid()) )
+        print("ticker_name= " + self.name, "|| ", "buy_date= " + str(self.buyDate), "|| " , "amount_of_shares= " + str(self.amount), "|| " , "total= " + str(self.total), "|| " , "fee= " + str(self.fee),"|| "
+              , "total_paid= " + str(self.total_paid()), "|| ", "stonks_days_left= " + str(self.stonksDateDayCount()) )
 
-    def tryKek(self):
-        print("fee= " + str(self.fee))
+    def stonksDate(self):
+        stonksDate = self.buyDate.replace(self.buyDate.year + 3)
+        return stonksDate
 
-    def getDate(self):
-        getDate = date(int(self.buyDate))
-        return getDate
+    def stonksDateDayCount(self):
+        d0 = date.today()
+        ##d0 = d0.strftime("%Y-%m-%d")
+        ##d0 = int(d0)
+        ##d0 = date(d0)
+        d1 = self.stonksDate()
+        dayCount = d1 - d0
+        return dayCount
 
-nakup0 = Trade("MMT", "2021, 10, 5", 93, 605, 7.95)
+nakup0 = Trade("MMT", date(2021, 10, 5), 93, 605.01, 7.95)
+nakup1 = Trade("LTC", date(2021, 9, 30), 34, 1085.41, 7.95)
+nakup2 = Trade("DX", date(2021, 9, 28), 40, 707.95, 7.95)
+nakup3 = Trade("PSEC", date(2021, 9, 28), 70, 557.45, 7.95)
+nakup4 = Trade("ARR", date(2021, 9, 28), 90, 985.35, 7.95)
 
-print(nakup0)
+
 nakup0.print_values()
-print(nakup0.fee)
-##nakup0.getDate()
+nakup1.print_values()
+nakup2.print_values()
+nakup3.print_values()
+nakup4.print_values()
 
-datumtest= date(2021, 10, 5)
-print(datumtest)
-print(type(datumtest))
+
+
+
+
+
+#print(nakup0.fee)
+#print(nakup0.buyDate)
+#print(nakup0.stonksDate())
+#print(nakup0.stonksDateDayCount())
