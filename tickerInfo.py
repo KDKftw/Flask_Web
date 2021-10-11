96fd896db336bdfa3161d3ea1bfe06a866b593bf
KDKftw <ooo.kadoun@gmail.com>
2021-10-11 19:53:36 +0200

    price, total is not actually total, new tickers

--- tickerInfo.py
+++ tickerInfo.py
@@ -1,10 +1,11 @@
 from datetime import date
 
 class Trade():
-    def __init__(self, name, buyDate, amount, total, fee):
+    def __init__(self, name, buyDate, amount, price, total, fee):
         self.name = name
         self.buyDate = buyDate
         self.amount = amount
+        self.price = price
         self.total = total
         self.fee = fee
 
@@ -13,7 +14,7 @@ class Trade():
         return(total_paid)
 
     def print_values(self):
-        print("ticker_name= " + self.name, "|| ", "buy_date= " + str(self.buyDate), "|| " , "amount_of_shares= " + str(self.amount), "|| " , "total= " + str(self.total), "|| " , "fee= " + str(self.fee),"|| "
+        print("ticker_name= " + self.name, "|| ", "buy_date= " + str(self.buyDate), "|| " , "amount_of_shares= " + str(self.amount),  "price_bought= " + str(self.price), "|| " , "total= " + str(self.total), "|| " , "fee= " + str(self.fee),"|| "
               , "total_paid= " + str(self.total_paid()), "|| ", "stonks_days_left= " + str(self.stonksDateDayCount()) )
 
     def stonksDate(self):
@@ -29,11 +30,26 @@ class Trade():
         dayCount = d1 - d0
         return dayCount
 
-nakup0 = Trade("MMT", date(2021, 10, 5), 93, 605.01, 7.95)
-nakup1 = Trade("LTC", date(2021, 9, 30), 34, 1085.41, 7.95)
-nakup2 = Trade("DX", date(2021, 9, 28), 40, 707.95, 7.95)
-nakup3 = Trade("PSEC", date(2021, 9, 28), 70, 557.45, 7.95)
-nakup4 = Trade("ARR", date(2021, 9, 28), 90, 985.35, 7.95)
+6,42
+31,69
+17,50
+7,85
+10,86
+
+
+
+nakup0 = Trade("MMT", date(2021, 10, 5), 93, 6.42, 605.01, 7.95)
+nakup1 = Trade("LTC", date(2021, 9, 30), 34, 31.69, 1085.41, 7.95)
+nakup2 = Trade("DX", date(2021, 9, 28), 40, 17.50, 707.95, 7.95)
+nakup3 = Trade("PSEC", date(2021, 9, 28), 70, 7.85, 557.45, 7.95)
+nakup4 = Trade("ARR", date(2021, 9, 28), 90, 10.86, 985.35, 7.95)
+
+bigDeal = [nakup0, nakup1, nakup2, nakup3, nakup4]
+
+nakup5 = Trade("O", date(2021, 9, 24), 11, 66.58, 740.33, 7.95)
+nakup6 = Trade("ORC", date(2021, 8, 20), 100, 4.9, 497.95, 7.95)
+
+
 
 
 nakup0.print_values()
@@ -41,7 +57,8 @@ nakup1.print_values()
 nakup2.print_values()
 nakup3.print_values()
 nakup4.print_values()
-
+nakup5.print_values()
+nakup6.print_values()
 
 
 
